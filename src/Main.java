2378891a0014b9f1bbf4cfaa438f9db2b5144d81
Aitor Couñago Figueroa <aitorcf@gmail.com>
2023-03-27 11:28:17 +0200

    Refactor: menus de selección

--- src/Main.java
+++ src/Main.java
@@ -10,32 +10,35 @@ public class Main {
                     "\n0 - Salir.");
             select = Input.num();
             if (select == 1) {
-                System.out.println(menu);
-                do {
-                    System.out.println("¿Desea guardar el menú?: " +
-                            "\n1 - Sí, guardalo. " +
-                            "\n0 - No, no lo guardes.");
-
-                    select = Input.num();
-                    if (select == 1) {
-                        File saveObject = new File();
-                        boolean confirm = saveObject.objectWriter(menu, "menu.txt", false);
-                        if (confirm) {
-                            System.out.println("Menú guardado.");
-                        } else {
-                            System.out.println("Error al guardar el menú.");
-                        }
-                        select = 0;
-                    }
-                } while (select != 0);
-
-                select = 1;
+                ShowMenu(menu);
             } else if (select == 2) {
                 menu = new Menu();
             } else if (select == 0) {
                 System.out.println("Hasta pronto!");
             } else {
                 System.out.println("Opción no disponible. Por favor, seleccione una de las opciones válidas.");
+            }
+        } while (select != 0);
+    }
+
+    public static void ShowMenu(Menu menu) {
+        System.out.println(menu);
+        int select;
+        do {
+            System.out.println("¿Desea guardar el menú?: " +
+                    "\n1 - Sí, guardalo. " +
+                    "\n0 - No, no lo guardes.");
+
+            select = Input.num();
+            if (select == 1) {
+                File saveObject = new File();
+                boolean confirm = saveObject.objectWriter(menu, "menu.txt", false);
+                if (confirm) {
+                    System.out.println("Menú guardado.");
+                } else {
+                    System.out.println("Error al guardar el menú.");
+                }
+                select = 0;
             }
         } while (select != 0);
     }
